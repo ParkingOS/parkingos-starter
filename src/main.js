b605ab259cf1f3225093a9dfac335b033dc9debb
Hunmanlife <562953415@qqcom>
2019-10-11 17:44:29 +0800

    20191011 push

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui';
-import '@/styles/common-variables.scss'
 import App from './App'
 import {router} from './router/index';
 import axios from 'axios'
